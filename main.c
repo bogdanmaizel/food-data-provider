74ad71fbf03eadc869d45484c4b7df9c465ded09
Bogdan Maizel <bogdan.maizel@student.utcluj.ro>
2019-11-15 11:24:49 +0200

    food-data-provider

--- main.c
+++ main.c
@@ -5,15 +5,14 @@
 #define MAX_FOOD_NAME 10
 #define MAX_MEAL_NAME 20
 #define MAX_LINE 25
-void readMeal(char * meal);
-float readNumber(char nr[]);
+
 int main() {
 
     //read no of foods
     int noOfFoods;
     printf("Please input number of food types\n");
     scanf("%d", &noOfFoods);
-
+    getchar();
     //read foods
     char **foods;
     foods = (char **) malloc(noOfFoods * sizeof(char *));
@@ -22,6 +21,7 @@ int main() {
         // read each FOOD
         foods[i] = (char *) malloc(MAX_FOOD_NAME * sizeof(char));
         scanf("%s", foods[i]);
+        getchar();
     }
 
     int *noOfmeals = (int *) malloc(noOfFoods * sizeof(int));
@@ -31,8 +31,8 @@ int main() {
             // read no of meals
             printf("Please input no of speciffic foods for food %s\n",foods[i]);
             scanf("%d",&noOfmeals[i]);
-
-            //read meals & prices
+            getchar();
+            //read meals & prices todo: figure this out
             meals[i] = (char**)malloc(noOfmeals[i]* sizeof(char*));
             prices[i] = (double*)malloc(noOfmeals[i]* sizeof(double));
 
@@ -42,11 +42,27 @@ int main() {
                 char line[MAX_LINE];
                 gets(line);
                 sscanf(line, "%s", meals[i][j]);
-                line[strlen(line)]='\0';
-                sscanf(line, "%1f", prices[i][j]);
+                line[strlen(line)-1]='\0';
+                line[strlen(meals[i][j]+1)]=line[strlen(meals[i][j]+2)]='\0';
+                sscanf(line + strlen(meals[i][j]) + 1, "%1f", prices[i][j]);
             }
 
         }
+    //read drinks
+    int noOfDrinks;
+    printf("Please input number of drinks.\n");
+    scanf("%d", noOfDrinks);
+    char **drinks=(char**)malloc(noOfDrinks*sizeof(char*));
+    double *drinksPrices=(double*)malloc(noOfDrinks* sizeof(double));
+    getchar();
+    printf("Please input the drinks: each line in the format <drink> (price):\n");
+    for (int i=0;i<noOfDrinks;++i) {
+        char line[MAX_LINE];
+        gets(line);
+        drinks[i]=(char*)malloc(MAX_FOOD_NAME* sizeof(char));
+        sscanf(line, "%s", drinks[i]);
+    }
+
 
     // display data
     printf("The data is:\n");
@@ -54,13 +70,13 @@ int main() {
         // display FOOD
         printf("%s: ", foods[i]);
         for(int j=0;j<noOfmeals[i];j++) {
-            printf("(%s, %.2lf) ",meals[i][j],prices[i][j]);
+            printf("(%s - %.2lf) ",meals[i][j],prices[i][j]);
         }
         printf("\n");
     }
 
     // free memory
-    for(int i=0;i<noOfFoods;i++) {
+    /*for(int i=0;i<noOfFoods;i++) {
         for(int j=0;j<noOfmeals;j++) {
             free(meals[i][j]);
         }
@@ -71,27 +87,8 @@ int main() {
     free(meals);
     free(prices);
     free(foods);
-    free(noOfmeals);
+    free(noOfmeals);*/
 
     return 0;
 }
 
-float readNumber(char nr[]) {
-    float x=0;
-    gets(nr);
-    for (int i=strlen(nr)-1;i>=0;--i) {
-        if (nr[i]>='0' && nr[i]<='9') x=x*10+(nr[i]-'0');
-    }
-}
-
-void readMeal(char * meal) {
-    char c = getchar();
-    int i=0;
-    while(c!='(') {
-        meal[i] = c;
-        c = getchar();
-        i++;
-    }
-    meal[i]='\0';
-    meal[i-1] = '\0';
-}
